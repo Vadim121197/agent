c690b01e23c1eb87ec8bbf30f26176355177dce0
Vadim121197 <vadimoleynik039@gmail.com>
2025-01-20 16:05:53 +0200

    change contract

--- src/lib/chains.ts
+++ src/lib/chains.ts
@@ -10,7 +10,7 @@ export enum Addresses {
 export const chainAddresses: Record<number, Record<Addresses, Address>> = {
 	[sepolia.id]: {
 		[Addresses.ETH_PRICE_FEED]: '0x694AA1769357215DE4FAC081bf1f309aDC325306',
-		[Addresses.POOL]: '0xE2E78001959aD45A785D168BdC38535602dAb225',
-		[Addresses.PAYMENT]: '0xE2E78001959aD45A785D168BdC38535602dAb225',
+		[Addresses.POOL]: '0x0388252Fa20de58E2C6CE927187185082AC002E9',
+		[Addresses.PAYMENT]: '0x0388252Fa20de58E2C6CE927187185082AC002E9',
 	},
 }
